f73204ec1466d425c23f5e29d180947c37d6b63e
Max <84030024+reubermax@users.noreply.github.com>
2023-09-06 22:37:59 +0200

    Repair models.py in db_acces folder

--- backend/db_access/models.py
+++ backend/db_access/models.py
@@ -3,20 +3,20 @@ from django.db import models
 # Create your models here.
 
 class Cash(models.Model):
-    cash_amount = models.DecimalFieldField(max_digits=6, decimal_places=2)
+    cash_amount = models.DecimalField(max_digits=6, decimal_places=2)
     date = models.DateField(auto_now_add=True)
-    type = models.CharField()
+    type = models.CharField(max_length=200)
 
 class Brand(models.Model):
-    brand_name = models.CharField()
+    brand_name = models.CharField(max_length=200)
     bottles_per_crate = models.PositiveSmallIntegerField()
 
 class Delivery(models.Model):
     date = models.DateField(auto_now_add=True)
-    brand = models.ForeignKey(Cash)
+    brand = models.OneToOneField(Cash, on_delete=models.DO_NOTHING)
 
 class Delivery_Brands(models.Model):
-    delivery_id = models.ForeignKey(Delivery)
-    brand_id = models.ForeignKey(Brand)
+    delivery_id = models.OneToOneField(Delivery, on_delete=models.DO_NOTHING)
+    brand_id = models.OneToOneField(Brand, on_delete=models.DO_NOTHING)
     crate_amount = models.IntegerField()
 
