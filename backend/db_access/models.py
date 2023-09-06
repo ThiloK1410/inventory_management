9618538b598f8ef6ef40a165590b21d465134069
Max <84030024+reubermax@users.noreply.github.com>
2023-09-06 23:09:56 +0200

    Change models.py

--- backend/db_access/models.py
+++ backend/db_access/models.py
@@ -2,18 +2,19 @@ from django.db import models
 
 # Create your models here.
 
-class Cash(models.Model):
+class Transaction(models.Model):
     cash_amount = models.DecimalField(max_digits=6, decimal_places=2)
     date = models.DateField(auto_now_add=True)
     type = models.CharField(max_length=200)
 
 class Brand(models.Model):
-    brand_name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    bottle_size = models.FloatField()
     bottles_per_crate = models.PositiveSmallIntegerField()
 
 class Delivery(models.Model):
     date = models.DateField(auto_now_add=True)
-    brand = models.OneToOneField(Cash, on_delete=models.DO_NOTHING)
+    cost = models.ForeignKey(Transaction, on_delete=models.DO_NOTHING)
 
 class Delivery_Brands(models.Model):
     delivery_id = models.OneToOneField(Delivery, on_delete=models.DO_NOTHING)
