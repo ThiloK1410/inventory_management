e7b5300b505e5fb40d04b3f8edff7db437f1d1dc
thilo <thilokloos@gmx.de>
2023-09-06 22:44:03 +0200

    fixed line endings

--- backend/db_access/models.py
+++ backend/db_access/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Cash(models.Model):
-    cash_amount = models.DecimalFieldField(max_digits=6, decimal_places=2)
+    cash_amount = models.DecimalField(max_digits=6, decimal_places=2)
     date = models.DateField(auto_now_add=True)
     type = models.CharField()
 
