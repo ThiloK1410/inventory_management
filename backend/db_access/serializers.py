5a2d2afa22c66c055c8a374e8a497e5d8bcfd216
Lukas <59834358+nabend187@users.noreply.github.com>
2023-09-07 22:52:22 +0200

    Change DeliverySerializer to allow inclusion of cost and brand_deliveries as JSON objects

--- backend/db_access/serializers.py
+++ backend/db_access/serializers.py
@@ -1,18 +1,39 @@
 from rest_framework import serializers
-from .models import Delivery, Brand, Transaction
-
-class DeliverySerializer(serializers.ModelSerializer):
-    brand = serializers.SlugRelatedField(slug_field="name", many=True, read_only=True)
-    class Meta:
-        model = Delivery
-        fields = '__all__'
-
-class BrandSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Brand
-        fields = "__all__"
+from .models import BrandDelivery, Delivery, Brand, Transaction
 
 class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
+        fields = "__all__"
+
+class BrandDeliverySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BrandDelivery
+        exclude = ("delivery",)
+
+class DeliverySerializer(serializers.ModelSerializer):
+    cost = TransactionSerializer()
+    brand_deliveries = BrandDeliverySerializer(many=True)
+
+    class Meta:
+        model = Delivery
+        fields = '__all__'
+    
+    def create(self, validated_data):
+        # Remove separately create fields from JSON
+        cost = Transaction.objects.create(**validated_data.pop("cost"))
+        brand_deliveries_list = validated_data.pop("brand_deliveries")
+
+        # Use the remaining JSON to create Delivery
+        delivery = Delivery.objects.create(cost=cost, **validated_data)
+
+        # Create BrandDelivery for every item in the brand_deliveries_list
+        for brand_delivery in brand_deliveries_list: 
+            BrandDelivery.objects.create(delivery=delivery, **brand_delivery)
+
+        return delivery
+
+class BrandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
         fields = "__all__"
