a5540e5e7603f48359af5a58847c1a0381d704d4
Lukas <59834358+nabend187@users.noreply.github.com>
2023-09-24 21:18:51 +0200

    Add incrementation of inventory

--- react/src/app/inventory/inventory.tsx
+++ react/src/app/inventory/inventory.tsx
@@ -1,5 +1,26 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { QuantityCard } from "../../components/quantity-card/quantity-card";
+import { API_URL } from "../../constants";
+import { InventoryItem } from "../../types/inventory-item";
+import Styles from "./inventory.module.css";
 
 export const Inventory: React.FunctionComponent = () => {
-  return <h1>Inventory</h1>;
+  const [items, setItems] = useState<InventoryItem[]>([]);
+
+  useEffect(() => {
+    axios.get(API_URL + "/inventory/").then(response => setItems(response.data));
+  }, []);
+
+  return (
+    <div className={Styles.items}>
+      {items.map(item => (
+        <QuantityCard
+          key={item.id}
+          text={item.brand.name}
+          detailText={`${item.brand.bottle_size}L`}
+          quantity={`${item.bottle_amount} bottles`}
+        />))}
+    </div>
+  );
 };
