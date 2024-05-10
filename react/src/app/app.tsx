3ea0bba12f97c19e3fa46a7a6269542a51ab3797
Max <84030024+docbob241@users.noreply.github.com>
2024-05-10 11:12:01 +0200

    Delete nav and start adding desktop ui design

--- react/src/app/app.tsx
+++ react/src/app/app.tsx
@@ -1,30 +1,30 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Deliveries } from "./deliveries/deliveries";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import { Deliveries } from "./deliveries/deliveries";
 import { Inventory } from "./inventory/inventory";
-import NavDropdown from "./nav-dropdown/nav-dropdown";
-import { NotFound } from "./not-found/not-found";
-import { Statistics } from "./statistics/statistics";
+// import { NotFound } from "./not-found/not-found";
+// import { Statistics } from "./statistics/statistics";
 
 import Styles from "./app.module.css";
 
-const navBarItems = [
-  { to: "/", title: "Inventory" },
-  { to: "/statistics", title: "Statistics" },
-  { to: "/deliveries", title: "Deliveries" },
-];
+// const navBarItems = [
+//   { to: "/", title: "Inventory" },
+//   { to: "/statistics", title: "Statistics" },
+//   { to: "/deliveries", title: "Deliveries" },
+// ];
 
 export const App: React.FunctionComponent = () => {
   return (
     <div className={Styles.main}>
-    <BrowserRouter>
-      <NavDropdown items={navBarItems} />
-      <Routes>
-        <Route path="*" element={<NotFound />} />
-        <Route path="/" element={<Inventory />} />
-        <Route path="statistics" element={<Statistics />} />
-        <Route path="deliveries/*" element={<Deliveries />} />
-      </Routes>
-    </BrowserRouter>
+      <h1 className={Styles.title}>Inventory</h1>
+      <Inventory />
+      {/* <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<NotFound />} />
+          <Route path="/" element={<Inventory />} />
+          <Route path="statistics" element={<Statistics />} />
+          <Route path="deliveries/*" element={<Deliveries />} />
+        </Routes>
+      </BrowserRouter> */}
     </div>
   );
 };
