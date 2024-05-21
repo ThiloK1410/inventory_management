b993021f755b15a007c2ce5f192e35e941569d6d
Max <84030024+docbob241@users.noreply.github.com>
2024-05-21 17:57:12 +0200

    Improve visibility of item card controls

--- react/src/app/inventory/item-card/item-card.tsx
+++ react/src/app/inventory/item-card/item-card.tsx
@@ -31,76 +31,74 @@ export const ItemCard: React.FunctionComponent<ItemCardProps> = props => {
     : props.item.bottle_amount;
 
   return (
-    <>
-      <div
-        onClick={props.setInFocus}
-        className={[
-          Styles.itemCard,
-          props.expanded ? Styles.expanded : "",
-          props.item.deleted ? Styles.deleted : "",
-        ].join(" ")}
-      >
-        <div className={Styles.header}>
-          <div className={Styles.label}>
-            <span className={Styles.brand}>{props.item.brand_name}</span>
-            <span className={Styles.itemDetails}>{details}</span>
-          </div>
-
-          <div className={Styles.quantity}>
-            <span>
-              {props.item.bottle_amount !== props.item.previousAmount && props.item.previousAmount}
-            </span>
-            <span>
-              {props.item.bottle_amount !== props.item.previousAmount && (
-                <FontAwesomeIcon
-                  className={
-                    props.item.bottle_amount > props.item.previousAmount
-                      ? Styles.increaseCaret
-                      : Styles.decreaseCaret
-                  }
-                  icon={faCaretRight}
-                />
-              )}
-            </span>
-            <span>{props.item.bottle_amount}</span>
-          </div>
+    <div
+      onClick={props.setInFocus}
+      className={[
+        Styles.itemCard,
+        props.expanded ? Styles.expanded : "",
+        props.item.deleted ? Styles.deleted : "",
+      ].join(" ")}
+    >
+      <div className={Styles.header}>
+        <div className={Styles.label}>
+          <span className={Styles.brand}>{props.item.brand_name}</span>
+          <span className={Styles.itemDetails}>{details}</span>
         </div>
-        <div className={Styles.cardControls}>
-          <div className={Styles.quantityControl}>
-            {crateAmount !== undefined && (
-              <div className={Styles.labelBox}>
-                <label>Crates</label>
-                <QuantityInput
-                  quantity={crateAmount}
-                  setQuantity={quantity =>
-                    props.setQuantity(quantity * props.item.crate_size! + bottleSurplus)
-                  }
-                />
-              </div>
+
+        <div className={Styles.quantity}>
+          <span>
+            {props.item.bottle_amount !== props.item.previousAmount && props.item.previousAmount}
+          </span>
+          <span>
+            {props.item.bottle_amount !== props.item.previousAmount && (
+              <FontAwesomeIcon
+                className={
+                  props.item.bottle_amount > props.item.previousAmount
+                    ? Styles.increaseCaret
+                    : Styles.decreaseCaret
+                }
+                icon={faCaretRight}
+              />
             )}
+          </span>
+          <span>{props.item.bottle_amount}</span>
+        </div>
+      </div>
+      <div className={Styles.cardControls}>
+        <div className={Styles.quantityControl}>
+          {crateAmount !== undefined && (
             <div className={Styles.labelBox}>
-              <label>Bottles</label>
+              <label>Crates</label>
               <QuantityInput
-                quantity={bottleSurplus}
+                quantity={crateAmount}
                 setQuantity={quantity =>
-                  props.setQuantity((crateAmount ?? 0) * (props.item.crate_size ?? 0) + quantity)
+                  props.setQuantity(quantity * props.item.crate_size! + bottleSurplus)
                 }
               />
             </div>
-          </div>
-          <div className={Styles.buttons}>
-            <Button rounded onClick={props.onCancel} className={Styles.cancelButton}>
-              Cancel
-            </Button>
-            <Button
-              rounded
-              onClick={props.onDelete}
-              severity="danger"
-              icon="fa-regular fa-trash-can"
+          )}
+          <div className={Styles.labelBox}>
+            <label>Bottles</label>
+            <QuantityInput
+              quantity={bottleSurplus}
+              setQuantity={quantity =>
+                props.setQuantity((crateAmount ?? 0) * (props.item.crate_size ?? 0) + quantity)
+              }
             />
           </div>
         </div>
+        <div className={Styles.buttons}>
+          <Button outlined rounded onClick={props.onCancel} className={Styles.cancelButton}>
+            Cancel
+          </Button>
+          <Button
+            rounded
+            onClick={props.onDelete}
+            severity="danger"
+            icon="fa-regular fa-trash-can"
+          />
+        </div>
       </div>
-    </>
+    </div>
   );
 };
